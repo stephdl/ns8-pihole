52f185a31ab0c8a3996eecfc4464fa722bd3b1ea
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-07-30 16:53:20 +0200

    fix(ui): ns8 module maintenance (#87)
    * build: update yarn to v4
    
    * build(ui): tune webpack chunk size and silence sass deprecations
    
    * chore(ui): disable no-console eslint rule
    
    * chore(ui): add format script to package.json
    
    * build: require core >= 3.20.1
    
    * build(ui): update axios
    
    * build(ui): update Vue to 2.7
    
    * build(ui): update ns8-ui-lib to latest 2.x
    
    * style(ui): format code with prettier

--- build-images.sh
+++ build-images.sh
@@ -29,7 +29,7 @@ buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
     nodebuilder-pihole \
-    sh -c "yarn install && yarn build"
+    sh -c "corepack enable && yarn install && yarn build"
 
 # Add imageroot directory to the container image
 buildah add "${container}" imageroot /imageroot
@@ -41,6 +41,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/pihole/pihole:2026.05.0" \
+    --label="org.nethserver.min-core=3.20.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
