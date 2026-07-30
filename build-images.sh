9693b18864b1b9a694c6987a2462bb778d2d8b66
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-07-30 16:17:03 +0200

    build: require core >= 3.20.1

--- build-images.sh
+++ build-images.sh
@@ -41,6 +41,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/pihole/pihole:2026.05.0" \
+    --label="org.nethserver.min-core=3.20.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
